21c0e65acd4f3ae589188d9d30f0b938f092a589
David Arques <david.darqsa@gmail.com>
2022-08-19 20:39:30 +0200

    Added connection to database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import connectDatabase from "./database/connectDatabase";
 import startServer from "./server/startServer";
 
 const port = process.env.PORT ?? 4000;
+const mongoDB = process.env.DDBB;
 
 (async () => {
   try {
+    await connectDatabase(mongoDB as string);
     await startServer(port as number);
   } catch {
     process.exit(1);
