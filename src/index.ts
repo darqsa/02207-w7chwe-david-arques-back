d5c262a9d0fa30f3218ff79925306d77501e4bd2
David Arques <david.darqsa@gmail.com>
2022-08-19 20:08:20 +0200

    Added start server function

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import startServer from "./server/startServer";
+
+const port = process.env.PORT ?? 4000;
+
+(async () => {
+  try {
+    await startServer(port as number);
+  } catch {
+    process.exit(1);
+  }
+})();
