978fa32c15e16330aabb1391ae0fe686c4d9a450
David Arques <david.darqsa@gmail.com>
2022-08-20 21:27:20 +0200

    Added registerUser test

--- src/server/controllers/models/User.ts
+++ src/server/controllers/models/User.ts
@@ -12,8 +12,6 @@ const userSchema = new Schema({
   },
   image: {
     type: String,
-    default:
-      "https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg",
   },
   friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   enemies: [{ type: Schema.Types.ObjectId, ref: "User" }],
--- src/server/controllers/usersController.test.ts
+++ src/server/controllers/usersController.test.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcryptjs";
 import User from "./models/User";
-import { getUsers } from "./usersController";
+import { getUsers, registerUser } from "./usersController";
+import createCustomError from "../../utils/createCustomError";
 
-describe("Given a getUsers controller", () => {
-  describe("When it recives a response", () => {
+describe("Given a getUsers function", () => {
+  describe("When it receives a response", () => {
     const req: Partial<Request> = {};
     const res: Partial<Response> = {
       status: jest.fn().mockReturnThis(),
@@ -29,6 +31,52 @@ describe("Given a getUsers controller", () => {
 
         expect(res.json).toHaveBeenCalledWith({ users });
       });
+    });
+  });
+});
+
+describe("Given a registerUser function", () => {
+  const fakeUser = {
+    nickName: "bob esponja",
+    password: "a",
+  };
+
+  const req: Partial<Request> = { body: fakeUser };
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next: Partial<NextFunction> = jest.fn();
+  describe("When it receives a response and a fake user", () => {
+    const bcryptHashTest = jest.fn().mockResolvedValue("test");
+    (bcrypt.hash as jest.Mock) = bcryptHashTest;
+
+    test("Then it should call status function with code 201", async () => {
+      const expectedStatus = 201;
+      User.create = jest.fn().mockResolvedValue(fakeUser);
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should invoke the response method json with a new User", async () => {
+      User.create = jest.fn().mockResolvedValue(fakeUser);
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.json).toHaveBeenCalledWith({ user: fakeUser });
+    });
+  });
+
+  describe("When it receives a response and a wrong fake user", () => {
+    test("Then it should call the next function and throw a customError", async () => {
+      const customError = createCustomError(400, "", "");
+      User.create = jest.fn().mockRejectedValue(customError);
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(customError);
     });
   });
 });
