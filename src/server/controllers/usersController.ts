fdd4cb1ba84807950e8998ad308e3ccc1d380c99
David Arques <david.darqsa@gmail.com>
2022-08-20 17:12:22 +0200

    Added error management for user register

--- src/server/controllers/usersController.ts
+++ src/server/controllers/usersController.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { IRegisterUser } from "../../types/userInterfaces";
 import { createHash } from "../../utils/auth";
+import createCustomError from "../../utils/createCustomError";
 import User from "./models/User";
 
 // eslint-disable-next-line import/prefer-default-export
@@ -10,11 +11,23 @@ export const getUsers = async (req: Request, res: Response) => {
   res.status(200).json({ users });
 };
 
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const user: IRegisterUser = req.body;
 
   user.password = await createHash(user.password);
-  // gestion de errores
-  const newUser = await User.create(user);
-  res.status(201).json({ user: newUser });
+  try {
+    const newUser = await User.create(user);
+    res.status(201).json({ user: newUser });
+  } catch (error) {
+    const customError = createCustomError(
+      400,
+      "Error creating user",
+      error.message
+    );
+    next(customError);
+  }
 };
